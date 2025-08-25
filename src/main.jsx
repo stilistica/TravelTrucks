321a0787fa6eff593e3f1d1a457f328492c56b3a
stillistica <rozhkovavika24@gmail.com>
2025-08-25 19:12:42 +0300

    add fronts, image

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './css/index.css'
+import './css/reset.css'
+import './css/fonts.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
