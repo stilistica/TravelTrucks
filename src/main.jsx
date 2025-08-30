1ed9621e63007c11364a78439ef83c631489c0a3
stillistica <rozhkovavika24@gmail.com>
2025-08-30 19:15:24 +0300

    add reset css

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './css/index.css'
 import './css/reset.css'
 import './css/fonts.css'
+import "modern-normalize";
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
