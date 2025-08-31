c84da562874dfee5378eee9fafec45081e56e03b
stillistica <rozhkovavika24@gmail.com>
2025-08-31 16:07:41 +0300

    add page notfound

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,12 +1,30 @@
-// import s from './NotFound.module.css';
+import { useEffect, useState } from "react";
+import s from "./NotFound.module.css";
+import { useNavigate } from "react-router-dom";
 
 function NotFound() {
+  const navigate = useNavigate();
+  const [countdown, setCountdown] = useState(5);
+
+  useEffect(() => {
+    if (countdown === 0) {
+      navigate("/", { replace: true });
+      return;
+    }
+
+    const interval = setTimeout(() => {
+      setCountdown((prev) => prev - 1);
+    }, 1000);
+
+    return () => clearTimeout(interval);
+  }, [countdown, navigate]);
 
   return (
-    <>
-
-    </>
-  )
+    <div className={s.notFound}>
+      <h1>Page Not Found</h1>
+      <p>Redirecting to the homepage in {countdown}...</p>
+    </div>
+  );
 }
 
 export default NotFound;
