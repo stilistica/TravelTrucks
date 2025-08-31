c13837973f1c1ed2c4904d8e36a56247233a0dc7
stillistica <rozhkovavika24@gmail.com>
2025-08-31 15:47:54 +0300

    add hero

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,26 @@
+import { useNavigate } from "react-router-dom";
+import Container from "../Container/Container";
+import s from "./Hero.module.css";
+import Button from "../Button/Button";
+
+const Hero = () => {
+  const navigate = useNavigate();
+  return (
+    <div className={s.hero}>
+      <Container>
+        <div className={s.container}>
+          <div className={s.text}>
+            <h1>Campers of your dreams</h1>
+            <p>You can find everything you want in our catalog</p>
+          </div>
+					<Button
+					onClick={() => navigate("/catalog")}>
+						View Now
+					</Button>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Hero;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 // import s from './HomePage.module.css';
 
+import Hero from "../../components/Hero/Hero";
+
 function HomePage() {
 
   return (
     <>
-
+    <Hero/>
     </>
   )
 }
