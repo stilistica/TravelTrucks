bc3954f8ec1b44ead660d24e3de567c236a93bd4
stillistica <rozhkovavika24@gmail.com>
2025-08-31 14:28:03 +0300

    header done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,29 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import Container from "../Container/Container";
 import s from "./Header.module.css";
 import sprite from "../../assets/icons/sprite.svg#icon-logo";
+import clsx from "clsx";
 
 const Header = () => {
+  const activeLink = ({ isActive }) => {
+    return clsx(s.link, isActive && s.active);
+  };
+
   return (
-    <header>
+    <header className={s.header}>
       <Container>
-        <div>
+        <div className={s.container}>
           <Link to="/">
-            <svg>
+            <svg className={s.svg}>
               <use href={`${sprite}#icon-logo`}></use>
             </svg>
           </Link>
-          <nav>
-            <NavLink to="/" className={buildLinkClass}>
+          <nav className={s.nav}>
+            <NavLink to="/" className={activeLink}>
               Home
             </NavLink>
-						<NavLink to="/catalog" className={buildLinkClass}>
-              Home
+            <NavLink to="/catalog" className={activeLink}>
+              Catalog
             </NavLink>
           </nav>
         </div>
