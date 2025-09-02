df7c97bec88a43f47fbab78a00e0e186f04db90c
stillistica <rozhkovavika24@gmail.com>
2025-09-02 19:58:15 +0300

    add persist

--- src/main.jsx
+++ src/main.jsx
@@ -7,14 +7,17 @@ import "modern-normalize";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./redux/store.js";
+import { persistor, store } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,27 @@
+import storage from "redux-persist/lib/storage";
 import { configureStore } from "@reduxjs/toolkit";
 import { campersReducer } from "./campers/slice";
+import { persistReducer, persistStore } from "redux-persist";
+import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 
+const persistConfig = {
+  key: "campers",
+  storage,
+  whitelist: ["favoriteCampers"],
+};
+
+const persistedReducer = persistReducer(persistConfig, campersReducer);
 
 export const store = configureStore({
-	reducer: {
-		campers: campersReducer, 
-	},
+  reducer: {
+    campers: persistedReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
