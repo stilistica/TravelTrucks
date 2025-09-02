ad2cd3a7f4d7c01c77e0b7d320b628d5b263b0cd
stillistica <rozhkovavika24@gmail.com>
2025-09-02 12:33:37 +0300

    add operations redux

---
+++ src/redux/campers/operations.js
@@ -0,0 +1,25 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { fetchCamperById, fetchCampers } from "../../utils/api";
+
+export const getCampers = createAsyncThunk(
+	"campers/getAll",
+	async (query, { rejectWithValue }) => {
+		try {
+			const data = await fetchCampers(query) // (query ?? {});
+			return data;
+		} catch (err) {
+			return rejectWithValue(err?.message || "Failed to load campers");
+		}
+	}
+)
+export const getCamperById = createAsyncThunk(
+	"campers/getById",
+	async (id, { rejectWithValue }) => {
+		try {
+			const data = await fetchCamperById(id);
+			return data;
+		} catch (err) {
+			return rejectWithValue(err?.message || "Failed to load camper");
+		}
+	}
+)
