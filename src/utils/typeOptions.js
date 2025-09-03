b7b509476d0bbd102b41dfd0e527188f881bf5a3
stillistica <rozhkovavika24@gmail.com>
2025-09-03 22:44:56 +0300

    add arr typeOptions

---
+++ src/utils/typeOptions.js
@@ -0,0 +1,7 @@
+import sprite from "../../assets/icons/sprite.svg";
+
+export const typeOptions = [
+	{ key: "panelTruck", label: "Panel Truck", iconUrl: `${sprite}#icon-van` },
+  { key: "fullyIntegrated", label: "Fully Integrated", iconUrl: `${sprite}#icon-fully` },
+  { key: "alcove", label: "Alcove", iconUrl: `${sprite}#icon-alcove` },
+]
