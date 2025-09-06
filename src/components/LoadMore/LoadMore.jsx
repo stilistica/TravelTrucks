e135fbc314e08987af27e43f10df8ffe707c6305
stillistica <rozhkovavika24@gmail.com>
2025-09-06 17:44:29 +0300

    add loadMore

---
+++ src/components/LoadMore/LoadMore.jsx
@@ -0,0 +1,13 @@
+import s from './LoadMore.module.css'
+
+const LoadMore = ({onClick, children}) => {
+	return (
+		<button 
+		className={s.btn}
+		onClick={onClick}>
+			{children}
+		</button>
+	)
+}
+
+export default LoadMore;
